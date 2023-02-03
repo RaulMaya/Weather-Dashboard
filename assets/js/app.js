119c7fede230e090f54712c21d86a8a1605bf67f
Raul <raulmayas20@gmail.com>
2023-02-02 20:36:10 -0600

    Selecting 5 different days

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,39 @@
-function getApi(city) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    console.log(latitude, longitude);
-    // var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
-    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        console.log(data.city.name);
-        console.log(data.list[0].main.temp);
-        var temperature = parseFloat(data.list[0].main.temp);
-        var city = data.city.name;
-        var country = data.city.country;
-        if (temperature > 20) {
-          var emoji = "☀️";
-        } else {
-          var emoji = "🥶";
-        }
-        statusEl.innerHTML =
-          city + ", " + country + " | " + temperature + " C° " + emoji;
-      });
-  }
+var inputCity = document.querySelector("input");
+var searchBtn = document.querySelector("button");
+
+function sendCity(event) {
+  event.preventDefault();
+  console.log(inputCity.value);
+  cityData(inputCity.value);
+}
+
+// function gettingCoords(city) {
+//   var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`;
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       getTemperatures(data[0].lat, data[0].lon) 
+//     });
+// }
+
+function cityData(city) {
+  //var requestUrl = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&units=metric`;
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data.list[3].dt_txt)
+        console.log(data.list[11].dt_txt)
+        console.log(data.list[19].dt_txt)
+        console.log(data.list[27].dt_txt)
+        console.log(data.list[35].dt_txt)
+    });
+}
+
+searchBtn.addEventListener("click", sendCity);
